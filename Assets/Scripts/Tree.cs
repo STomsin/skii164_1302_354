f98d11010eb96abcf2c85b3aad1dc79f0a18eae6
STomsin <yodpat.k@gmail.com>
2026-08-25 21:33:58 +0700

    game DDDDD

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -1,49 +1,37 @@
-using System.Net;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Tree : MonoBehaviour
 {
     private MeshRenderer rd;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rd = GetComponent<MeshRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
-        rd.material.color = Color.red;
-        Player player = collision.gameObject.GetComponent<Player>();
+        if (rd != null) rd.material.color = Color.red;
+
+        Player player = collision.gameObject.GetComponentInParent<Player>();
 
         if (player == null)
             return;
 
         player.HP -= 25;
-
         UIManager.instance.ShowNotiText($"Hurt -25\nHP: {player.HP}");
-
 
         if (player.HP <= 0)
         {
             player.HP = 0;
-            UIManager.instance.ShowNotiText($"YOu Dead \n Your hp is: {player.HP}");
+            UIManager.instance.ShowNotiText($"You Dead \n Your hp is: {player.HP}");
             Time.timeScale = 0f;
             UIManager.instance.ShowHideRestartButton(true);
         }
-
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        rd.material.color = new Color32(118, 104, 97, 255);
+        if (rd != null) rd.material.color = new Color32(118, 104, 97, 255);
     }
-
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,32 +3,15 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField]
-
-    private TMP_Text notiText;
-
-    [SerializeField]
-    private GameObject restartButton;
-
-    [SerializeField]
-    private Player player;
+    [SerializeField] private TMP_Text notiText;
+    [SerializeField] private GameObject restartButton;
+    [SerializeField] private Player player;
 
     public static UIManager instance;
 
     void Awake()
     {
         instance = this;
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 
     public void ShowNotiText(string s)
@@ -40,10 +23,10 @@ public class UIManager : MonoBehaviour
     {
         player.transform.position = new Vector3(0, 90, -85);
         player.HP = 100;
-        ShowNotiText("Restart");
+        player.Point = 0; // รีเซ็ตคะแนนผู้เล่นใหม่เมื่อเริ่มเกม
+        ShowNotiText("Start!");
         Time.timeScale = 1f;
         ShowHideRestartButton(false);
-
     }
 
     public void ShowHideRestartButton(bool flag)
