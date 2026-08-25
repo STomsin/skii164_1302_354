d4e1e3d48c0b13b911020b49362b9aa1171e1753
STomsin <yodpat.k@gmail.com>
2026-08-25 15:15:19 +0700

    first game play

---
+++ Assets/Scripts/Tree.cs
@@ -0,0 +1,38 @@
+using System.Net;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    private MeshRenderer rd;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rd = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        rd.material.color = Color.red;
+        Player player = collision.gameObject.GetComponent<Player>();
+
+        if (player == null)
+            return;
+
+        player.HP -= 25;
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        rd.material.color = new Color32(118, 104, 97, 255);
+    }
+
+}
