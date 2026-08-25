330102bb3e735ff3453da3f3fdea8bf5e248e516
STomsin <yodpat.k@gmail.com>
2026-08-25 15:58:47 +0700

    finish gate

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -28,6 +28,17 @@ public class Tree : MonoBehaviour
 
         player.HP -= 25;
 
+        UIManager.instance.ShowNotiText($"Hurt -25\nHP: {player.HP}");
+
+
+        if (player.HP <= 0)
+        {
+            player.HP = 0;
+            UIManager.instance.ShowNotiText($"YOu Dead \n Your hp is: {player.HP}");
+            Time.timeScale = 0f;
+            UIManager.instance.ShowHideRestartButton(true);
+        }
+
     }
 
     private void OnCollisionExit(Collision collision)
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+
+    private TMP_Text notiText;
+
+    [SerializeField]
+    private GameObject restartButton;
+
+    [SerializeField]
+    private Player player;
+
+    public static UIManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ShowNotiText(string s)
+    {
+        notiText.text = s;
+    }
+
+    public void RestartGame()
+    {
+        player.transform.position = new Vector3(0, 90, -85);
+        player.HP = 100;
+        ShowNotiText("Restart");
+        Time.timeScale = 1f;
+        ShowHideRestartButton(false);
+
+    }
+
+    public void ShowHideRestartButton(bool flag)
+    {
+        restartButton.SetActive(flag);
+    }
+}
